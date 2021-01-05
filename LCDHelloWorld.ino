4e9d6708b5b3fe62d588efe879ef9cd91cdaaa56
ishangarg19 <76923937+ishangarg19@users.noreply.github.com>
2021-01-05 10:16:16 -0500

    Add files via upload

---
+++ LCDHelloWorld.ino
@@ -0,0 +1,19 @@
+
+#include <Adafruit_LiquidCrystal.h>
+
+#include <Wire.h>
+
+Adafruit_LiquidCrystal lcd(0);
+
+void setup() 
+{
+ lcd.begin(16,2);
+ lcd.print("Hello World!");
+ lcd.setCursor (7,1);
+ lcd.print(":)");
+}
+
+void loop() 
+{
+ //nothing
+}
---
+++ LCDHellowWorldButton.ino
@@ -0,0 +1,33 @@
+const int button = 2;
+int val = 0;
+
+#include <Adafruit_LiquidCrystal.h>
+#include <Wire.h>
+Adafruit_LiquidCrystal lcd(0);
+
+void setup() 
+{
+ pinMode(button, INPUT);
+ lcd.begin(16,2);
+ 
+ val = digitalRead(button);
+ 
+ if(val == HIGH)
+ {
+   lcd.print("Hello World!"); 
+   return;
+ }
+ else
+ {
+   lcd.print("_");
+   return;
+ }
+}
+
+
+void loop()
+{
+ 
+ 
+ 
+}
